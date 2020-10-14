583c727d59c7c5f197ad73948d4169a2e32ec548
TokenHung <72561139+TokenHung@users.noreply.github.com>
2020-10-14 19:03:42 -0400

    Add MainMenu script
    Just add Start() and Quit() function
    Option function is not yet done

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// add this api
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        // When Unity Build, Scenes are queued with Index (like 0.GameMenu, 1.GameScene ...)
+        SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Update is called once per frame
+    public void QuitGame()
+    {
+        Application.Quit();
+        // Debug message for preview
+        print("Now you press QUIT");
+    }
+}
