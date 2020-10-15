44182ea9ab5d527407dfca8e9e4b8187dd8bc4ae
TokenHung <72561139+TokenHung@users.noreply.github.com>
2020-10-15 01:10:37 -0400

    add project and drop function
    create infinity prefab to project(or drop) since inventory is not finished

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -9,8 +9,15 @@ public class player : MonoBehaviour
     public float runSpeed = 10f;
     public float climbSpeed = 1f;
     public Vector2 jumpHeight = new Vector2(0f,12f);
+    public Vector2 projectile_force = new Vector2(10f,10f);
+    private float face_direction; // record where player faces. because transform.forward doesn't work in 2D
     public float naturalGravity;
     public int jumpback;
+    public GameObject dropped_item; // prefab of dropped item
+    private GameObject cur_dropped_item; //  current dropped item
+
+    public GameObject projectile;
+    private GameObject cur_projectile;
     [HideInInspector] public rope rope;
     [HideInInspector] public bool climb = false;
 
@@ -27,7 +34,7 @@ public class player : MonoBehaviour
         naturalGravity = rb.gravityScale;
         bar = GameObject.Find("bar").GetComponent<bar>();
         startPos = transform.position;
-
+        face_direction = 1.0f;
     }
 
     // Update is called once per frame
@@ -42,16 +49,20 @@ public class player : MonoBehaviour
         if (transform.position.y <= -10f){
             Restart();
         }
-
-
     }
-    
     void FixedUpdate ()
     {
         float inputX = Input.GetAxis("Horizontal");
         float inputY = Input.GetAxis("Vertical");
+        float inputX_Raw = Input.GetAxisRaw("Horizontal"); // record face direction
         float posY = transform.position.y;
         float posX = transform.position.x;
+
+        // detect where player faces
+        if (inputX_Raw != 0f){
+            face_direction = inputX_Raw;
+        }
+
         //jump
         if (climb && inputY != 0){
             float ropeX = rope.transform.position.x;
@@ -70,7 +81,16 @@ public class player : MonoBehaviour
         if(inputX != 0){
             Vector3 move = new Vector3(runSpeed * inputX , posY, 0);     
             move *= Time.deltaTime;
-            transform.Translate(move);   
+            transform.Translate(move);
+        }
+        
+        // call drop and project here because we may use inputX 
+        if (Input.GetButtonDown("Fire1")){
+            DropItem();
+        }
+
+        if (Input.GetButtonDown("Fire2")){
+            ProjectItem(face_direction);
         }
 
         // if (Input.GetButtonDown("Crouch")){
@@ -80,6 +100,20 @@ public class player : MonoBehaviour
         // }
     }
 
+    private void DropItem(){
+        cur_dropped_item = Instantiate(dropped_item, transform.position, Quaternion.identity);
+        Rigidbody2D cur_dropped_rb = cur_dropped_item.GetComponent<Rigidbody2D>();
+        cur_dropped_rb.angularVelocity = 0f;
+    }
+
+    private void ProjectItem(float face_direction){
+        // need last moving direction to determine project
+        cur_projectile = Instantiate(projectile, transform.position + new Vector3 (0.0f,0.5f,0.0f), Quaternion.identity);
+        Rigidbody2D cur_projectile_rb = cur_projectile.GetComponent<Rigidbody2D>();
+        cur_projectile_rb.angularVelocity = 0f;
+        cur_projectile_rb.AddForce(new Vector2(projectile_force.x *face_direction,projectile_force.y));
+    }
+
     private void OnCollisionEnter2D(Collision2D other) {
         // print(other.gameObject.tag);
         if (other.gameObject.tag== "Ground"){
@@ -87,6 +121,9 @@ public class player : MonoBehaviour
         }
     }
 
+    private void dropItem(){
+
+    }
     public void touchMonster(int direction){
         Vector2 layback;
         layback = new Vector2(direction*jumpback,8);
