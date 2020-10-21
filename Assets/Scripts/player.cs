7b3e8b92502685b38a5be1a535f64230cf5c35e7
TokenHung <72561139+TokenHung@users.noreply.github.com>
2020-10-20 21:31:10 -0700

    function : trajectory line
    Bug : 
    1. line won't stop when hitting the ground
    2. It's not generated during charging

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,9 +12,7 @@ public class player : MonoBehaviour
     public Vector2 jumpHeight = new Vector2(0f,12f);
     public int moveForce = 5;
     public int jumpForce = 5;
-    public Vector2 projectile_force = new Vector2(1.0f,1.0f); // projectile (thrown item) force
-    private float charged_time = 1.0f; // for accumulated projectile charged time. 0 will become "drop".
-    private float face_direction = 1.0f; // record where player faces. because transform.forward doesn't work in 2D
+
     public float naturalGravity;
     public GameObject dropped_item; // prefab of dropped item
     private GameObject cur_dropped_item; //  current dropped item
@@ -30,12 +29,24 @@ public class player : MonoBehaviour
     private bar bar;
     private healthSystem healthSystem = new healthSystem(100);
     // Start is called before the first frame update
+
+    [Header("Projectile")]
+    public Vector2 projectile_force = new Vector2(1.0f,1.0f); // projectile (thrown item) force
+    private float charged_time = 1.0f; // for accumulated projectile charged time. 0 will become "drop".
+    private float face_direction = 1.0f; // record where player faces. because transform.forward doesn't work in 2D
+    public LineRenderer projectile_line; // trajectory
+    public int point_number; // trajectory's point number
+    public Vector2 projectile_velocity; // for newton formula
+    public float ground_y;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         naturalGravity = rb.gravityScale;
         bar = GameObject.Find("bar").GetComponent<bar>();
         startPos = transform.position;
+        projectile_line.useWorldSpace = true;
+        float ground_y = -3.37f;
     }
 
     // Update is called once per frame
@@ -51,7 +62,7 @@ public class player : MonoBehaviour
         if (transform.position.y <= -10f || transform.position.y >= 100f){
             transform.position = startPos;
         }
-
+ 
         // MuseButton 0: left ; 1 : right.        
         // count accumlated charged time for projectile
         if (Input.GetMouseButton(0)){
@@ -107,13 +118,57 @@ public class player : MonoBehaviour
     
     // project prefab from (player's coordinate + some hight)
     private void ProjectItem(float face_direction, Vector2 charged_projectile_force){
-        // need last moving direction to determine project
+        Vector2 temp_velocity = new Vector2((charged_projectile_force.x * face_direction)/25, charged_projectile_force.y/ 25);
+        // Vector2 temp_velocity = new Vector2((charged_projectile_force.x * face_direction)/cur_projectile_rb.mass, charged_projectile_force.y/ cur_projectile_rb.mass);
+        StartCoroutine(DrawTrajectory(temp_velocity));
         cur_projectile = Instantiate(projectile, transform.position + new Vector3 (0.0f,0.5f,0.0f), Quaternion.identity);
         Rigidbody2D cur_projectile_rb = cur_projectile.GetComponent<Rigidbody2D>();
         cur_projectile_rb.angularVelocity = 0f;
-        cur_projectile_rb.AddForce(new Vector2(charged_projectile_force.x * face_direction, charged_projectile_force.y));
+        //cur_projectile_rb.AddForce(new Vector2(charged_projectile_force.x * face_direction, charged_projectile_force.y));
+        cur_projectile_rb.velocity += temp_velocity;
+        print("rb.velocity"+cur_projectile_rb.velocity);
+    
     }
     
+    private IEnumerator DrawTrajectory(Vector2 prefab_velocity){
+        projectile_line.positionCount = point_number;
+        projectile_line.SetPositions(TrajectoryGenerator(prefab_velocity));
+        print("inDrawFunction");
+        yield return null;
+    }
+
+    private Vector3[] TrajectoryGenerator(Vector2 prefab_velocity){
+        Vector3[] Generated_points = new Vector3[point_number];
+        point_number = (point_number == 0)? 200 : point_number; // avoid divided by 0
+        float point_density = Newton_Trajectory_HitGround_Time(ground_y, prefab_velocity).x / point_number;
+        for (int i = 0; i < point_number; ++i){
+            //float time_in_Newton = (float)(i / point_number); // input t for Newton Vo*t + 0.5*a*t^2
+            float time_in_Newton = point_density * i;
+            Generated_points[i] = Newton_Trajectory_Coordinate(time_in_Newton, prefab_velocity);
+        }
+        //print("last_point"+Generated_points[(int)((point_density * point_number)-2)]);
+        return  Generated_points;
+    }
+
+    // classic Newton formula x = Vo*t + 0.5*a*t^2 
+    private Vector2 Newton_Trajectory_Coordinate(float time_in_Newton, Vector2 prefab_velocity){
+        float X = transform.position.x + prefab_velocity.x * time_in_Newton ;
+        float Y = transform.position.y + prefab_velocity.y * time_in_Newton + 0.5f * Physics2D.gravity.y * time_in_Newton * time_in_Newton; // gravityScale can be added, g = -9.8f
+        return new Vector2 (X, Y);
+    }
+
+    // calculate projectile destination (when hit ground).
+    private Vector2 Newton_Trajectory_HitGround_Time(float ground_y, Vector2 prefab_velocity){
+        // Y = V0(y) * t + 0.5 * a * t^2 ; (V0, Y, a) are all constants
+        // Y is ground_y - projectile_ini_y, so use  -(b / 2*a) +/- (b^2-4ac)^0.5 to solve
+        // temp_1 is (-b / 2*a); temp_2 is (b^2-4ac)^0.5 >>> temp_1 is highest point time; ans is hit ground time.
+        float Y = ground_y - transform.position.y;
+        float temp_1 = -prefab_velocity.y / (Physics2D.gravity.y); // can add gravity scale
+        float temp_2 = Mathf.Pow(Mathf.Pow(prefab_velocity.y, 2) + 2 * Physics2D.gravity.y * Y ,0.5f);
+        float ans = (temp_2 < 0)? (temp_1 - temp_2) : (temp_1 + temp_2);
+        return new Vector2(ans, temp_1);
+    }
+
     // drop prefab from player's coordinate
     private void DropItem(){
         cur_dropped_item = Instantiate(dropped_item, transform.position, Quaternion.identity);
@@ -127,10 +182,6 @@ public class player : MonoBehaviour
             print(other.gameObject.tag);
             jump = true;
         }
-    }
-
-    private void dropItem(){
-
     }
     public void touchMonster(int direction, int damageAmount){
         Vector2 layback = new Vector2(direction*moveForce,jumpForce);
