6fd4eb9209cc85a15386e0b7b97f8bb752b8c106
TokenHung <hungtzukuan@gmail.com>
2020-11-12 21:17:18 -0500

    add shampoo particle

---
+++ Assets/Scripts/objectShampoo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectShampoo : MonoBehaviour
+{
+    private objectScript objectScript;
+    public GameObject bubble;
+    private GameObject cur_bubble;
+    private player player_script;
+    private GameObject player;
+    void Start()
+    {
+        objectScript = new objectScript(gameObject);
+        player = GameObject.Find("player");
+        player_script = player.GetComponent<player>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag== "Ground"){
+            cur_bubble = Instantiate(bubble, transform.position, Quaternion.identity);
+        }
+    }
+}
