1f0581da0939cc9d428bf4e9d917da92170b4f6e
christine59487 <christine5487@gmail.com>
2020-11-13 23:31:41 +0800

    StoryScene

--- Assets/Scripts/Inventory/Story1.cs
+++ Assets/Scripts/Inventory/Story1.cs
@@ -8,33 +8,59 @@ public class Story1 : MonoBehaviour
     public Text text1;
     public int speed;
     private string[] lines = new string[] { "便利商店停電??", "怎麼…這是?!", "我在哪", "你來到這裡，卻還不知道，前方，要面對的是什麼", "逃是唯一的出路" };
+    public Sprite[] sprite = new Sprite[6];
+    public Image image;
     private int i;
 
+    /*timer版本*/
+    /* void Start()
+     {
+         i = 0;
+         InvokeRepeating("timer", 1, speed);
+
+
+     }
+
+     private void Update()
+     {
+         if (Input.GetKeyDown(KeyCode.Space))
+         {
+
+         }
+     }
+
+     void timer()
+     {
+         text1.text = lines[i];
+         i++;
+         if (i >= 5)
+         {
+             i = 0;
+             CancelInvoke("timer");
+             SceneManager.LoadScene("SampleScene");
+
+         }
+
+     }*/
 
     void Start()
     {
-       
-        /*lastTime = 0f;
-        timepassed = 0;
-        draw = false;*/
         i = 0;
-        InvokeRepeating("timer", 1, speed);
-
-
+        image.sprite = sprite[i];
     }
 
-
-    void timer()
+    private void Update()
     {
-        text1.text = lines[i];
-        i++;
-        if (i >= 5)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            i = 0;
-            CancelInvoke("timer");
-            SceneManager.LoadScene("SampleScene");
-
+            i++;
         }
+
+        if (i >= 5)
+            SceneManager.LoadScene("Cave");
+
+        image.sprite = sprite[i];
+        text1.text = lines[i];
 
     }
 }
---
+++ Assets/Scripts/Inventory/showInfo.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class showInfo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Collider2D Obcollider;
+    public GameObject text1;
+    public bool MouseOn;
+    public Item.ItemType Type;
+
+    void Start()
+    {
+        text1.SetActive(false);
+        MouseOn = false;
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        
+    }*/
+
+    private void OnMouseOver()
+    {
+        MouseOn = true;
+    }
+
+    public void SetType(Item.ItemType type)
+    {
+      /*   Type = type;
+        switch (type)
+        {
+            default:
+            case Item.ItemType.Bomb_L:
+                {
+                    //text1.GetComponent<Text>() = "r";
+                } */
+         /*   case Item.ItemType.Bomb_S: ItemAssets.Instance.Bomb_SSprite;
+            case Item.ItemType.Bomb_Timer:ItemAssets.Instance.Bomb_TimerSprite;
+            case Item.ItemType.Teddy:
+            case Item.ItemType.TransferGate: ItemAssets.Instance.TransferGateSprite;
+            case Item.ItemType.Lego: ItemAssets.Instance.LegoSprite;
+            case Item.ItemType.CardBoard:
+            case Item.ItemType.Bottle: ItemAssets.Instance.BottleSprite;
+            case Item.ItemType.Carton: ItemAssets.Instance.CartonSprite;
+            case Item.ItemType.Pillow:
+                {
+                    Debug.Log("found"); ItemAssets.Instance.PillowSprite;
+                }
+
+        }*/
+    }
+}
