0154c05c89269deb69a8388dc07ae81ccc1925fe
christine59487 <christine5487@gmail.com>
2020-11-20 17:49:50 +0800

    LevelChoose

---
+++ Assets/Scripts/Inventory/SetGameLevel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetGameLevel : MonoBehaviour
+{
+    public GameLevel Level;
+    public GameObject panelEnterName;
+    public GameObject panelNow;
+    public enum GameLevel
+    {
+        Easy,
+        Normal,
+        Hard,
+    }
+    // Start is called before the first frame update
+    public void SetLevelEasy()
+    {
+        Level = GameLevel.Easy; 
+        panelEnterName.SetActive(true);
+        panelNow.SetActive(false);
+    }
+
+    public void SetLevelNormal()
+    {
+        Level = GameLevel.Normal;
+        panelEnterName.SetActive(true);
+        panelNow.SetActive(false);
+    }
+
+    public void SetLevelHard()
+    {
+        Level = GameLevel.Hard;
+        panelEnterName.SetActive(true);
+        panelNow.SetActive(false);
+    }
+}
