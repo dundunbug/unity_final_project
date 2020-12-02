64fc21e81b77ba2962b3472c5ea1d2241bca5810
christine59487 <christine5487@gmail.com>
2020-12-03 03:10:17 +0800

    ScorePage

--- Assets/ScorePage.cs
+++ Assets/ScorePage.cs
@@ -2,19 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ScorePage : MonoBehaviour
 {
+    public TextMeshProUGUI T1;
+    public TextMeshProUGUI T2;
+    public TextMeshProUGUI T3;
+    public GameData gameData;
     // Start is called before the first frame update
     void Start()
     {
-
+        gameData = GameObject.Find("GameData").GetComponent<GameData>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        T1.text = gameData.Name;
+        //T2.text = gameData.Name;
+        T3.text = gameData.min+" min "+gameData.PlayTime+" sec";
     }
 
     public void Continue()
