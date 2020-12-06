5d78bef959298b217145b0888a720a92ae8b14ec
TokenHung <hungtzukuan@gmail.com>
2020-12-06 07:44:56 -0800

    fix: Discard legacy animation and use Update()

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -5,34 +5,54 @@ using UnityEngine;
 public class DoorTrigger : MonoBehaviour
 {
     public GameObject door;
+    private bool down = false;
     private Animation door_animation;
     private Animation m_animation;
     private int done = 0;
+    private bool DestroyFlag = true;
     // Start is called before the first frame update
     void Start()
     {
-        if(door != null)
-        {
-            door_animation = door.GetComponent<Animation>();
-        }
-        m_animation = gameObject.GetComponent<Animation>();    
+        //if(door != null)
+        //{
+        //    door_animation = door.GetComponent<Animation>();
+        //}
+        //m_animation = gameObject.GetComponent<Animation>();    
     }
 
     void OnTriggerStay2D (Collider2D col)
     {
         if (done == 0 && col.gameObject.tag == "Player")
         {
-            if (door_animation != null && (col.gameObject.transform.position.x - transform.position.x) < 0.02)
+            /*if (door_animation != null && (col.gameObject.transform.position.x - transform.position.x) < 0.1)
             {
                 m_animation.Play("ButtonDown");
                 door_animation.Play("OpenDoor");
                 done = 1;
-            }
+            }*/
+            if ((col.gameObject.transform.position.x - transform.position.x) < 0.1)
+            down = true;
+
         }
     }
     // Update is called once per frame
     void Update()
     {
+        if (down == true)
+        {
+            OpenDoor();
+        }
         
+    }
+    void OpenDoor()
+    {
+        if(DestroyFlag)
+        {
+            Destroy(gameObject, 5.0f);
+            Destroy(door, 5.0f);
+            DestroyFlag = false;
+        }
+        gameObject.transform.position -= new Vector3 (0.0f, 0.005f, 0.0f);
+        door.transform.position -= new Vector3 (0.0f, 0.025f, 0.0f);
     }
 }
