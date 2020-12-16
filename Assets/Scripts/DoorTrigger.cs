730d2b683d8e6438aca48c3d580b2ca7486d81f6
TokenHung <hungtzukuan@gmail.com>
2020-12-16 07:16:37 -0800

    change button layer and z axis

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -52,7 +52,7 @@ public class DoorTrigger : MonoBehaviour
             Destroy(door, 5.0f);
             DestroyFlag = false;
         }
-        gameObject.transform.position -= new Vector3 (0.0f, 0.005f, 0.0f);
-        door.transform.position -= new Vector3 (0.0f, 0.025f, 0.0f);
+        gameObject.transform.position -= new Vector3 (0.0f, 0.01f, 0.0f);
+        door.transform.position -= new Vector3 (0.0f, 0.03f, 0.0f);
     }
 }
